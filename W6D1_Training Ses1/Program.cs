3e57c0f4ea1f851143c7737ddb11295761a8adbb
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-11 10:10:34 +0800

    Complex Numbers

--- W6D1_Training Ses1/Program.cs
+++ W6D1_Training Ses1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,9 @@ namespace W6D1_Training_Ses1
     {
         static void Main(string[] args)
         {
+            Complex complex = new Complex(12.65, 6.69);
+            Console.WriteLine("Complex number is {0}" , complex);
+            Console.ReadLine();
         }
     }
 }
