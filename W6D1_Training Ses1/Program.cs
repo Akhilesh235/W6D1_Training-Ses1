094e3234286bb051c04ab5beaccb5e987bfcb867
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-11 10:12:02 +0800

    Adding Complex Numbers

--- W6D1_Training Ses1/Program.cs
+++ W6D1_Training Ses1/Program.cs
@@ -11,8 +11,13 @@ namespace W6D1_Training_Ses1
     {
         static void Main(string[] args)
         {
-            Complex complex = new Complex(12.65, 6.69);
-            Console.WriteLine("Complex number is {0}" , complex);
+            Complex complex1 = new Complex(12.65, 6.69);
+            Console.WriteLine("Complex number is {0}" , complex1);
+
+            Complex complex2 = new Complex(8, 4);
+            Console.WriteLine("2nd Complex number is {0}", complex2);
+
+            Console.WriteLine("sum of 2 complex number " + (complex1+complex2));
             Console.ReadLine();
         }
     }
